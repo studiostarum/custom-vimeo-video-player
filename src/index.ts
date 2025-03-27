33cc0cf8768fafc0e7db779816799f9e414ff89c
studiostarum <dev@starum.studio>
2025-03-27 16:02:13 +0100

    Import CSS directly in the entry file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
+// Import components
+import { VideoPlayer } from './components/video-player';
+import { Lightbox } from './components/lightbox';
+
+// Import styles directly
+import './components/video-player/index.css';
+import './components/lightbox/index.css';
+import './components/shared/video-lightbox.css';
+
 // Export components
-export { VideoPlayer } from './components/video-player';
-export { Lightbox } from './components/lightbox';
+export { VideoPlayer, Lightbox };
 
 // Export types
 export type { VideoPlayerOptions } from './components/video-player/types';
-export type { LightboxOptions } from './components/lightbox/types';
-
-// Export styles
-import './components/video-player/index.css';
-import './components/lightbox/index.css';
-import './components/shared/video-lightbox.css'; 
+export type { LightboxOptions } from './components/lightbox/types'; 
